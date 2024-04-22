31f70b47413f360a01f3f53c570adf0e85d7f9d5
vargaae <vargaae@hotmail.com>
2024-04-22 14:43:32 +0200

    100320-Plan the Endpoints

--- server.js
+++ server.js
@@ -10,3 +10,14 @@ app.listen(3000, () => {
   console.log("app is running on port 3000");
 });
 
+/*
+
+/ --> res = this is working
+AUTHENTICATION
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
++/profile/:userId --> PUT = user - if we want to modify the user account
+/image --> PUT --> user - if we want to add Rank or other information of the user
+
+*/
